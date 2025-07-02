698929583d2b19ae579db602c674724a6982b2c2
LHSmicius <lukasmic@gmail.com>
2025-07-03 00:32:24 +0300

    chore: Add serde_yaml dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+
+struct CanMessage {
+    id: u32,
+    name: String,
+    length: u32,
+    periodicity: u32,
+}
+
+fn main() -> Result<(), serde_yaml::Error> {
+    println!("PSA-RE-CLIENT");
+    let can_message = CanMessage {
+        id: 0x208,
+        name: "GENERIC_ENGINE_DATA".to_string(),
+        length: 8,
+        periodicity: 10,
+    };
+    
+    let yaml = serde_yaml::to_string(&can_message)?;
+    println!("{}", yaml);
+
+    let parsed_message: CanMessage = serde_yaml::from_str(&yaml)?;
+    println!("{:?}", parsed_message);
+
+    Ok(())
 }
